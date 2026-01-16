6d7ae8e3485124c3daea24960d338d0ada9a9812
khawla687 <k_titki@live.concordia.ca>
2026-01-15 23:29:38 -0500

    commit 2

--- src/Array.java
+++ src/Array.java
@@ -6,43 +6,35 @@ public class Array {
         items = new int[size];
         count = 0;
     }
-
     void Append(int item) {
         if (count == items.length) {
             System.out.println("Array full");
-            return;
         } else {
             items[count] = item; // items[count++]= item;
             count++;
         }
     }
-
     void Traverse() {
         for (int i = 0; i < items.length; i++)
             System.out.println(items[i]);
     }
-
     boolean Search(int item) {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < items.length; i++) {
             if (items[i] == item)
                 return true;
         }
         return false;
     }
-
     public int getCount() {
         return count;
     }
 
-    // ===== ADDED ONLY (nothing above changed) =====
-    public static void main(String[] args) {
+    public static void main(int[] args) {
         Array arr = new Array(5);
         arr.Append(10);
         arr.Append(20);
         arr.Append(30);
-
         arr.Traverse();
-
         System.out.println("Found 20? " + arr.Search(60));
         System.out.println("Count = " + arr.getCount());
     }
