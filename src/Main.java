2884a40e07ce21397d85eaaedab57917f34df556
khawla687 <k_titki@live.concordia.ca>
2026-01-15 19:02:09 -0500

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a test project!");
+        System.out.println("Hello, this is first pull!");
+    }
+
+}
